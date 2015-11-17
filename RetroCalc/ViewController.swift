a0d7d78dcea65b03dca79ea7a2819483752733e2
Nicholas Smith <npsmith1990@gmail.com>
2015-11-16 21:10:17 -0600

    Connect buttons to outlets

--- RetroCalc/ViewController.swift
+++ RetroCalc/ViewController.swift
@@ -11,24 +11,61 @@ import AVFoundation
 
 class ViewController: UIViewController {
     
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "eee"
+    }
+    
     @IBOutlet weak var outputLbl: UILabel!
     
     var btnSound: AVAudioPlayer!
     
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-    @IBAction func numberPressed(btn: UIButton!) {
+        
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
         
     }
 
+    @IBAction func numberPressed(btn: UIButton!) {
+        btnSound.play()
+        
+        runningNumber += "\(btn.tag)"
+    }
+    
+
+    @IBAction func onDividePressed(sender: AnyObject) {
+    }
+
+    @IBAction func onMultiplyPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onMinusPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onPlusPressed(sender: AnyObject) {
+    }
+    
+    @IBAction func onEqualPressed(sender: AnyObject) {
+    }
 }
 
